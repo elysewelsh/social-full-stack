f9377be16983806a921a9b40f6d9f09870d33898
elysewelsh <elysecwelsh@gmail.com>
2026-03-25 11:34:00 -0400

    made custom provider

---
+++ frontend/src/context/userContext.js
@@ -0,0 +1,21 @@
+import { createContext, useState } from 'react'
+
+const UserContext = createContext(null)
+
+function UserProvider({ children }) {
+
+    const [user, setUser] = useState(null)
+
+    const value = {
+        user,
+        setUser
+    }
+
+    return (
+    <UserContext.Provider value={value}>
+        {children}
+    </UserContext.Provider>
+    )
+}
+
+export default UserProvider
