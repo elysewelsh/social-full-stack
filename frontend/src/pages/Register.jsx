9e70876b46ce051d4c2ff5f1f1020b6eff591690
elysewelsh <elysecwelsh@gmail.com>
2026-03-25 09:55:24 -0400

    added form on register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,35 @@
+import { useState } from 'react'
+
 function Register() {
+
+    const [form, setForm] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <div>
-            Register Page
+            <h1>Register Page</h1>
+        <form>
+            <label htmlFor="username">username:</label>
+            <input value={form.username} id="username" name="username" type="text" required />
+            
+            <label htmlFor="email">email:</label>
+            <input value={form.email} id="email" name="email" type="text" required />
+            
+            <label htmlFor="password">password:</label>
+            <input value={form.password} id="password" name="password" type="text" required />
+
+            <button>Register</button>
+        </form>
         </div>
     )
 }
