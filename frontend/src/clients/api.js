937efb39bef4deafe1770114453df220198ebbd7
elysewelsh <elysecwelsh@gmail.com>
2026-03-25 14:46:35 -0400

    getting all posts in console

--- frontend/src/clients/api.js
+++ frontend/src/clients/api.js
@@ -1,14 +1,17 @@
 import axios from 'axios'
 
-let token = localStorage.getItem('token')
-
 export const userClient = axios.create({
     baseURL: 'http://localhost:3000/api/users'
 })
 
 export const postClient = axios.create({
-    baseURL: 'http://localhost:3000/api/posts',
-    headers: {
-        Authorization: `Bearer ${token}`
+    baseURL: 'http://localhost:3000/api/posts'
+})
+
+postClient.interceptors.request.use((req) => {
+    let token = localStorage.getItem('token')
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`
     }
+    return req
 })
